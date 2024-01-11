823eaa7113688a8f3fde79f3b4e0aa902dbbeeef
«Oleg» <Pan159dom159@yandex.ru>
2024-01-11 22:15:02 +0300

    Lesson assignment

--- src/notebook/view/UserView.java
+++ src/notebook/view/UserView.java
@@ -38,6 +38,10 @@ public class UserView {
                 case UPDATE:
                     String userId = prompt("Enter user id: ");
                     userController.updateUser(userId, createUser());
+
+                case LIST:
+                    System.out.println(userController.readAll());
+
             }
         }
     }
